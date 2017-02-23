9baddc66ac4c909052d0020e9377bb8633f114f4
BeatrixSzekeres <rixi1125@gmail.com>
2017-02-23 12:29:27 +0100

    classok hozzaadva

---
+++ ConsoleApplication2/D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    public class D : B
+    {
+        public void S7()
+        {
+            Console.WriteLine("D:S7");
+            S3();
+        }
+
+        public void S8()
+        {
+            Console.WriteLine("D:S8");
+            base.V2();
+        }
+
+        protected virtual void V2()
+        {
+            Console.WriteLine("D:V2");
+            S7();
+        }
+    }
+}
